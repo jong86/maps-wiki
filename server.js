da3d52f203343716f53de5ba6b4cefb9353277c9
SpinnyFinny <finn.hiltner@hotmail.com>
2017-10-25 20:59:31 -0700

    routes for /maps and /pins; included dummy data

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const knexLogger = require('knex-logger');
 
 // Seperated Routes for each Resource
 const usersRoutes = require('./routes/users');
+const mapRoutes = require('./routes/maps');
+const pinRoutes = require('./routes/pins');
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -40,6 +42,8 @@ app.use(express.static('public'));
 
 // Mount all resource routes
 app.use('/api/users', usersRoutes(knex));
+app.use('/maps', mapRoutes());
+app.use('/pins', pinRoutes());
 
 // Home page
 app.get('/', (req, res) => {
