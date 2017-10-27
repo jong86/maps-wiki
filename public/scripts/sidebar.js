7f33f1022b109f1a421a5b48240dc4073f5b9de9
kelvin8wong <kelvin8wong@gmail.com>
2017-10-27 00:18:17 +0000

    added event handler for search bar

--- public/scripts/sidebar.js
+++ public/scripts/sidebar.js
@@ -1,5 +1,16 @@
 $(function() {
     $(".sidebar-toggle" ).click(function() {
       $(".sidebar-nav").animate({width: 'toggle'});
-    });
+    })
+
+    $('.form-control').change( function () {
+        var filter = $(this).val();
+        console.log(filter)
+        if (filter) {
+          $('.map-list').find("a:not(:contains(" + filter + "))").parent().slideUp();
+          $('.map-list').find("a:contains(" + filter + ")").parent().slideDown();
+        } else {
+          $('.map-list').find("li").slideDown();
+        }
+    })
 })
