d82a170a430b1aacfdbe7496693c3aa6852e650f
SpinnyFinny <finn.hiltner@hotmail.com>
2017-10-27 16:10:23 -0700

    fixed session typo causing if condition to fail on post to /pins

--- routes/pins.js
+++ routes/pins.js
@@ -23,8 +23,11 @@ module.exports = function (db) {
   });
 
   pinRoutes.post('/', function (req, res) {
-    //  creates new pin
-    //  some dummy data
+    if (!req.session.user_id) {
+      console.log(req.session);
+      console.log("Can't create pins if not logged in");
+      return;
+    }
     const newPin = req.body;
     const pin = {
       latitude: newPin.latitude,
@@ -48,22 +51,21 @@ module.exports = function (db) {
   });
 
   pinRoutes.put('/:id', function (req, res) {
-    const pinId = req.params.id;
-    //  update pin if user is authorized
-    //  dummmyy data
+    if (!req.session.user_id) {
+      console.log("Can't edit pins if not logged in");
+      return;
+    }
+    const updatedPin = req.body;
     const pin = {
-      id: 0,
-      latitude: '49.2827',
-      longitude: '-123.1207',
-      title: 'Official Vancouver Co-ordinates Cafe',
-      description: 'A good cafe with no beans',
-      image: 'http://www.fillmurray.com/200/300',
-      url: 'https://www.youtube.com/watch?v=IzVqkV_hQjc',
-      user_id: 0,
-      type_id: 0,
-      map_id: 0,
-      created_at: Date.now(),
-      version: Date.now()
+      latitude: updatedPin.latitude,
+      longitude: updatedPin.longitude,
+      title: updatedPin.title,
+      description: updatedPin.description,
+      image: updatedPin.image,
+      url: updatedPin.url,
+      user_id: req.session.user_id,
+      type_id: updatedPin.type_id,
+      map_id: updatedPin.map_id
     };
     res.json(pin);
   });
